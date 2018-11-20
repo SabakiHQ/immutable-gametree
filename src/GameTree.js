96045700d2ce3db2fc54754c164400f33093d742
Yichuan Shen <shen.yichuan@gmail.com>
2018-11-20 21:56:12 +0100

    More gametree methods

--- src/GameTree.js
+++ src/GameTree.js
@@ -70,6 +70,17 @@ class GameTree {
         return tree
     }
 
+    navigate(id, step, currents) {
+        step = Math.sign(step)
+
+        let node = this.get(id)
+        if (step < 0) return this.get(node.parentId)
+
+        return currents[node.id] != null
+            ? this.get(currents[node.id])
+            : node.children[0] || null
+    }
+
     *listNodes() {
         function* inner(node) {
             yield node
@@ -82,10 +93,67 @@ class GameTree {
         yield* inner(this.root)
     }
 
+    *listNodesHorizontally(startId, step) {
+        step = Math.sign(step) || 1
+
+        let level = this.getLevel(startId)
+        let section = [...this.getSection(level)]
+        let index = section.findIndex(node => node.id === startId)
+
+        while (index >= 0) {
+            while (0 <= index && index < section.length) {
+                yield section[index]
+                index += step
+            }
+
+            level += step
+            section = [...this.getSection(level)]
+            index = step > 0 ? 0 : section.length - 1
+        }
+    }
+
+    *listCurrentNodes(currents) {
+        let node = this.root
+
+        while (node != null) {
+            yield node
+            node = this.navigate(node.id, 1, currents)
+        }
+    }
+
+    *listMainNodes() {
+        yield* this.listCurrentNodes({})
+    }
+
     getLevel(id) {
         if (id === this.root.id) return 0
 
         return this.getLevel(this.get(id).parentId) + 1
+    }
+
+    *getSection(level) {
+        if (level === 0) {
+            yield this.root
+            return
+        }
+
+        for (let parent of this.getSection(level - 1)) {
+            yield* parent.children
+        }
+    }
+
+    getCurrentHeight(currents) {
+        let inner = node => {
+            if (node == null) return 0
+
+            let child = currents[node.id] == null
+                ? node.children[0]
+                : node.children.find(child => child.id === currents[node.id])
+
+            return 1 + inner(child)
+        }
+
+        return inner(this.root)
     }
 
     getHeight() {
@@ -95,6 +163,23 @@ class GameTree {
         }
 
         return this._heightCache
+    }
+
+    onCurrentLine(id, currents) {
+        let node = this.get(id)
+        let {parentId} = node
+
+        return parentId == null
+            || (
+                node.id === currents[parentId]
+                || currents[parentId] == null
+                && this.get(parentId).children[0] === node
+            )
+            && this.onCurrentLine(parentId, currents)
+    }
+
+    onMainLine(id) {
+        return this.onCurrentLine(id, {})
     }
 
     toJSON() {
