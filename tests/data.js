a80dd154d303bd0f702e72b15b2acd7fbe0ffd02
Yichuan Shen <shen.yichuan@gmail.com>
2019-01-03 14:24:51 +0100

    Supply test tree with a merger function

--- tests/data.js
+++ tests/data.js
@@ -1,7 +1,18 @@
 const GameTree = require('..')
 
+let merger = (node, data) => {
+    if (
+        node.data.B != null && data.B != null && node.data.B[0] === data.B[0]
+        || node.data.W != null && data.W != null && node.data.W[0] === data.W[0]
+    ) {
+        return data
+    }
+
+    return null
+}
+
 let id1, childId1, childId2, childId3, subChildId1
-let tree = new GameTree().mutate(draft => {
+let tree = new GameTree({merger}).mutate(draft => {
     id1 = draft.appendNode(draft.root.id, {B: ['dd']})
     childId1 = draft.appendNode(id1, {W: ['dq'], MA: ['qd', 'qq']})
     childId2 = draft.appendNode(id1, {W: ['qd']})
